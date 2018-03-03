fc6d1f748372492170fc6a0aa9d0867886765914
adrianalucescu <adriana.lucescu@pionix.ro>
2018-03-03 18:50:46 +0200

    bookmarks components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { BookmarksComponent } from './components/bookmarks/bookmarks.component';
+import { AddBookmarkComponent } from './components/add-bookmark/add-bookmark.component';
+import { BookmarkListComponent } from './components/bookmark-list/bookmark-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookmarksComponent,
+    AddBookmarkComponent,
+    BookmarkListComponent
   ],
   imports: [
     BrowserModule

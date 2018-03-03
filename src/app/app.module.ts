3b05441b07b284075442c01fe32e9f4b7c809bef
adrianalucescu <adriana.lucescu@pionix.ro>
2018-03-03 19:13:20 +0200

    wip at bookmarks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
-import { BookmarksComponent } from './components/bookmarks/bookmarks.component';
-import { AddBookmarkComponent } from './components/add-bookmark/add-bookmark.component';
-import { BookmarkListComponent } from './components/bookmark-list/bookmark-list.component';
+import {AppComponent} from './app.component';
+import {BookmarksComponent} from './components/bookmarks/bookmarks.component';
+import {AddBookmarkComponent} from './components/add-bookmark/add-bookmark.component';
+import {BookmarkListComponent} from './components/bookmark-list/bookmark-list.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    BookmarksComponent,
-    AddBookmarkComponent,
-    BookmarkListComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        BookmarksComponent,
+        AddBookmarkComponent,
+        BookmarkListComponent
+    ],
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        FormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/components/add-bookmark/add-bookmark.component.ts
+++ src/app/components/add-bookmark/add-bookmark.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
-  selector: 'app-add-bookmark',
-  templateUrl: './add-bookmark.component.html',
-  styleUrls: ['./add-bookmark.component.css']
+    selector: 'app-add-bookmark',
+    templateUrl: './add-bookmark.component.html',
+    styleUrls: ['./add-bookmark.component.css']
 })
 export class AddBookmarkComponent implements OnInit {
 
-  constructor() { }
+    form: FormGroup;
 
-  ngOnInit() {
-  }
+    constructor(formBuilder: FormBuilder) {
+        this.form = formBuilder.group({
+            // 'title': ['Test'],
+            // 'link': ['google.com']
+        });
+    }
+
+    ngOnInit() {
+    }
+
+    addBookmark(data) {
+        console.log(data);
+    }
+
 
 }
